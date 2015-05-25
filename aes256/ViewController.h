01007799b1b0d654ba32bc18098f65260fc07318
Richey <liqi1234@foxmail.com>
2015-05-25 16:50:12 +0800

    Initial Commit

---
+++ aes256/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  aes256
+//
+//  Created by Richey on 15/5/25.
+//  Copyright (c) 2015年 Richey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ aes256/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  aes256
+//
+//  Created by Richey on 15/5/25.
+//  Copyright (c) 2015年 Richey. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
