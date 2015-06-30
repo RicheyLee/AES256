58cdc6c86b510718c203e29ef076994531da8b39
Richey <liqi1234@foxmail.com>
2015-06-30 16:57:52 +0800

    init commit

--- aes256/ViewController.m
+++ aes256/ViewController.m
@@ -7,7 +7,8 @@
 //
 
 #import "ViewController.h"
-
+#import "NSData+Base64.h"
+#import "NSData+AES256.h"
 @interface ViewController ()
 
 @end
@@ -16,6 +17,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    NSString *key = @"abcdef1234567890";
+    
+    
+    NSString *str = [NSString stringWithFormat:@"[{\"departid\":\"2\",\"department\":\"\u7f51\u7edc\u90e8\"}]"];
+    
+    NSData *plain = [str dataUsingEncoding:NSUTF8StringEncoding];
+    NSData *eData = [plain AES256EncryptWithKey:key];
+    NSString *secretString = [eData base64EncodedString];
+    NSLog(@"encrypted string:%@",secretString);
+    
+    
+    
+    
+    NSString *enString = [NSString stringWithString:secretString];
+    NSData *cipher = [NSData dataFromBase64String:enString];
+    NSData *bDecrypt = [cipher AES256DecryptWithKey:key];
+    NSString *dateString = [[NSString alloc] initWithData:bDecrypt encoding:NSASCIIStringEncoding];
+    NSLog(@"decrypted string:%@",dateString);
+
+    
+    
+    
+    
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
